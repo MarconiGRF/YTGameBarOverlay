5b5050a4416ea0afc2ab2eb4bd4847b9a9243f65
Marconi Gomes <mgrf@cin.ufpe.br>
2020-05-25 04:21:09 -0300

    Cleans the unused namespaces.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,33 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Windows.Devices.SmartCards;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Networking.Sockets;
-using Windows.Storage;
-using Windows.Storage.Streams;
-using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using YoutubeGameBarWidget;
 using YoutubeGameBarWidget.WebServer;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace YoutubeGameBarOverlay {
     /// <summary>
--- Webpage.xaml.cs
+++ Webpage.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace YoutubeGameBarWidget
 {
@@ -67,6 +53,8 @@ namespace YoutubeGameBarWidget
                         }
                     );
         }
+
+        
 
         /// <summary>
         /// Handles the keypresses on Webpage's main grid.
